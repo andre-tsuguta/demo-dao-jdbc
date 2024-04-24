0957628806ecf9a367c32a11f9b777229ac81bef
Andre Tsuguta <andre.mat@gmail.com>
2024-04-24 15:16:50 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,12 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id = " + newSeller.getId());
 		
+		System.out.println("\n === Test 5 - Seller Update ===");
+		seller = sellerDao.findById(1);
+		seller.setEmail("bobbrown@gmail.com");
+		sellerDao.update(seller);
+		
+		System.out.println("Update Completed!");
 		
 	}
 
