378400bad156899a4abf661f4bafefa3bcb8e1ba
Andre Tsuguta <andre.mat@gmail.com>
2024-04-24 14:47:09 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -26,11 +27,17 @@ public class Program {
 		}
 		
 		System.out.println("\n === Test 3 - Seller findAll ===");
-	    list = sellerDao.findAll();
+        list = sellerDao.findAll();
 		
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n === Test 4 - Seller insert ===");
+		Seller newSeller = new Seller(null,"Jack Orange", "jack@gmail.com", new Date(), 4000.00, dep);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New Id = " + newSeller.getId());
+		
 		
 	}
 
